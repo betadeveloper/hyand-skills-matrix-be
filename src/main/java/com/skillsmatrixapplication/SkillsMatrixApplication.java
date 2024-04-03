e4ec857959898f160bd5f5759bceff33495ba5a0
GOD\rtautkus <raigardas.tautkus@god.dev>
2024-04-03 15:03:55 +0300

    initial commit

---
+++ src/main/java/com/skillsmatrixapplication/SkillsMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.skillsmatrixapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsMatrixApplication.class, args);
+	}
+
+}
